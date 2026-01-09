2ca8d19de530c26ccb9c0d3b8d8f520f8877fbd2
kyanyanya <dinhphduc281@gmail.com>
2026-01-09 09:31:03 +0700

    fix

--- src/components/Chatbot.jsx
+++ src/components/Chatbot.jsx
@@ -16,20 +16,20 @@ const Chatbot = () => {
 
         script.onload = () => {
             import('https://cdn.jsdelivr.net/npm/@n8n/chat/dist/chat.bundle.es.js').then(({ createChat }) => {
-                // src/components/Chatbot.jsx
+                
                 createChat({
                     webhookUrl: 'https://kyanyanya.app.n8n.cloud/webhook/80fb0d87-4125-47c3-bc00-5f66c53396ad/chat',
 
-                    // SỬA DÒNG "Hi there!" THÀNH TÊN TRANG WEB CỦA BẠN
+            
                     title: 'AI Travel Tours',
 
-                    // SỬA DÒNG "Start a chat..." THÀNH LỜI CHÀO MỚI
+                    
                     subtitle: 'Chào bạn, chúc bạn một ngày mới tốt lành.',
 
-                    showWelcomeScreen: true, // Nếu bạn muốn ẩn hẳn mảng đen này, hãy đổi thành false
+                    showWelcomeScreen: true, 
                     mainColor: '#8233ff',
 
-                    // Các tin nhắn xuất hiện trong bong bóng chat
+                    
                     initialMessages: [
                         'Xin chào! Tôi có thể giúp gì cho chuyến đi sắp tới của bạn?'
                     ]
